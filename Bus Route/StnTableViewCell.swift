592266e97a96448b1c15fab01ebd8dc3c0ca98ad
LOH CHIN GUAN <lcg1989@hotmail.com>
2016-12-08 11:34:38 +0800

    add info icon

--- Bus Route/StnTableViewCell.swift
+++ Bus Route/StnTableViewCell.swift
@@ -13,15 +13,31 @@ class StnTableViewCell: UITableViewCell {
     @IBOutlet weak var stationLabel: UILabel!
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var busNumberLabel: UILabel!
+    var delegate: StnTableViewCellDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    @IBOutlet weak var infoButton: UIButton! {
+        didSet {
+            infoButton.addTarget(self, action: #selector(onInfoButtonPressed), for: .touchUpInside)
+        }
+    }
+    
+    func onInfoButtonPressed(button: UIButton) {
+        delegate?.StnTableViewCellOnInfoButtonPressed(cell: self)
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
 
+}
+
+protocol StnTableViewCellDelegate {
+    func StnTableViewCellOnInfoButtonPressed(cell : StnTableViewCell)
 }
